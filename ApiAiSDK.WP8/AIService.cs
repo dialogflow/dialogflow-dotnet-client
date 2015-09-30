40d1822c5bfc5b346b5e4d7173b8c81e352c842f
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-09-30 17:53:55 +0600

    Add classes to WP8 version with special features

---
+++ ApiAiSDK.WP8/AIService.cs
@@ -0,0 +1,112 @@
+﻿//
+//  API.AI SDK - client-side libraries for API.AI
+//  =================================================
+//
+//  Copyright (C) 2015 by Speaktoit, Inc. (https://www.speaktoit.com)
+//  https://www.api.ai
+//
+//  ***********************************************************************************************************************
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+//  the License. You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//  specific language governing permissions and limitations under the License.
+//
+//  ***********************************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiAiSDK.Model;
+using ApiAiSDK.Util;
+
+namespace ApiAiSDK
+{
+    public abstract class AIService
+    {
+        public static AIService CreateService(AIConfiguration config)
+        {
+            return new SystemRecognitionService(config);
+        }
+
+        /// <summary>
+        /// Still under development....
+        /// </summary>
+        protected virtual event Action<float> AudioLevelChanged;
+
+        /// <summary>
+        /// Event fired on the success processing result received from server
+        /// </summary>
+        public virtual event Action<AIResponse> OnResult;
+
+        /// <summary>
+        /// Event will fire if an error appears
+        /// </summary>
+        public virtual event Action<AIServiceException> OnError;
+
+        /// <summary>
+        /// Initialize service, call this method before any requests
+        /// </summary>
+        /// <returns></returns>
+        public abstract Task InitializeAsync();
+        
+        /// <summary>
+        /// Start listening
+        /// </summary>
+        /// <returns></returns>
+        public abstract Task StartRecognitionAsync(RequestExtras requestExtras = null);
+        
+        /// <summary>
+        /// Cancel all listening and request processes
+        /// </summary>
+        public abstract void Cancel();
+
+        /// <summary>
+        /// Send simple text request. This method does not use OnResult and OnSuccess callbacks for simplicity
+        /// </summary>
+        /// <param name="text">Request in text form</param>
+        /// <param name="requestExtras">Optional request parameters such as Entities and Contexts</param>
+        /// <returns>Server response</returns>
+        /// <exception cref="ArgumentNullException">If text null or empty</exception>
+        /// <exception cref="AIServiceException">If any error appears while request</exception>
+        public async Task<AIResponse> TextRequestAsync(string text, RequestExtras requestExtras = null)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException("text");
+            }
+
+            return await TextRequestAsync(new AIRequest(text, requestExtras));
+        }
+
+        /// <summary>
+        /// Send simple text request. This method does not use OnResult and OnSuccess callbacks because for simplicity
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Server response</returns>
+        /// <exception cref="ArgumentNullException">If text null or empty</exception>
+        /// <exception cref="AIServiceException">If any error appears while request</exception>
+        public abstract Task<AIResponse> TextRequestAsync(AIRequest request);
+
+        protected virtual void FireOnResult(AIResponse response)
+        {
+            OnResult.InvokeSafely(response);
+        }
+
+        protected virtual void FireOnError(AIServiceException aiException)
+        {
+            OnError.InvokeSafely(aiException);
+        }
+
+        protected virtual void OnAudioLevelChanged(float level)
+        {
+            AudioLevelChanged.InvokeSafely(level);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-api.ai: .NET Core Library
+api.ai: .NET Library
 ==============
 
 [![Build Status](https://travis-ci.org/api-ai/api-ai-net.svg?branch=master)](https://travis-ci.org/api-ai/api-ai-net)
 [![Nuget Version](https://img.shields.io/nuget/v/ApiAiSDK.svg)](https://www.nuget.org/packages/ApiAiSDK/)
 
-The api.ai .NET Core Library makes it easy to integrate the [API.AI natural language processing API](http://api.ai) into your .NET application. API.AI allows using voice commands and integration with dialog scenarios defined for a particular agent in API.AI.
+The api.ai .NET Library makes it easy to integrate the [API.AI natural language processing API](http://api.ai) into your .NET application. API.AI allows using voice commands and integration with dialog scenarios defined for a particular agent in API.AI.
 
 Library provides simple programming interface for making text and voice requests to the API.AI service. 
 
@@ -16,7 +16,7 @@ Library can be installed with Nuget
 PM> Install-Package ApiAiSDK
 ```
 
-Or can be downloaded in zip from the [Releases](https://github.com/api-ai/api-ai-net/releases) page.
+Or can be downloaded as sources from the [Releases](https://github.com/api-ai/api-ai-net/releases) page.
 
 ### Usage
 
@@ -53,6 +53,102 @@ Done! Now you can easily do requests to the API.AI service
     ```
 
 Also see [unit tests](https://github.com/api-ai/api-ai-net/blob/master/ApiAiSDK.Tests/ApiAiTest.cs) for more examples.
+
+## Windows Phone 8 version
+
+Windows Phone version has some additional features such as system speech recognition for easy API.AI service integration.
+After installing the library you should add permissions for Internet and Sound recording to your app.
+Currently, speech recognition is performed using Windows Phone System speech recognition. So, you must be sure languages you are using is installed on device (It can be checked on Settings->speech screen of device).
+
+To use special features you need to use `AIService` class instead of `ApiAi` class. 
+
+### Initialization 
+
+First, you need to initialize AIConfiguration object with your keys and desired language.
+
+```csharp
+var config = new AIConfiguration("subscription key", "client access token", SupportedLanguage.English);
+```
+
+Second, create AIService object using the configuration object.
+
+```csharp
+var aiService = AIService.CreateService(config);
+```
+
+Now you need add handlers for OnResult and OnError events
+
+```csharp
+aiService.OnResult += aiService_OnResult;
+aiService.OnError += aiService_OnError;
+```
+
+And at the end call Initialization method
+
+```csharp
+await aiService.InitializeAsync();
+```
+
+The entire code snippet:
+
+```csharp
+try
+{
+    var config = new AIConfiguration("subscription key", "client access token", SupportedLanguage.English);
+
+    aiService = AIService.CreateService(config);
+    aiService.OnResult += aiService_OnResult;
+    aiService.OnError += aiService_OnError;
+    await aiService.InitializeAsync();
+}
+catch (Exception e)
+{
+    // Some exception processing
+}
+```
+
+### Using API.AI
+
+Now you can use methods for listening and requesting results from server, all you need to call `StartRecognitionAsync` method (don't forget to use `await` operator, otherwise you will not be able to catch some processing exceptions)
+
+```csharp
+try
+{
+    await aiService.StartRecognitionAsync();
+}
+catch (Exception exception)
+{
+    // Some exception processing
+}
+```
+
+### Results processing
+
+Results will be passed to the `OnResult` handler, most of errors will be passed to the `OnError` handler. Don't forget to use dispatcher when working with UI, because of handlers can be called from the Background thread.
+
+```csharp
+void aiService_OnError(AIServiceException error)
+{
+    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+    {
+        // sample error processing
+        ResultTextBlock.Text = error.Message;
+    });
+}
+
+void aiService_OnResult(ApiAiSDK.Model.AIResponse response)
+{
+    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+    {
+        // sample result processing
+        ResultTextBlock.Text = response.Result.ResolvedQuery;
+    });
+}
+```
+
+## Windows 10 version
+
+Work approach is similar to Windows Phone version.
 
 ## Open Source Project Credits
 
