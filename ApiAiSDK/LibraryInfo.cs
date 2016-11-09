24f4a9f1d571e39f99c375dbb1f0904af250c52a
Danil Skachkov <danil.skachkov@speaktoit.com>
2016-11-09 16:02:41 +0700

    Release 1.6.4

--- ApiAiSDK/LibraryInfo.cs
+++ ApiAiSDK/LibraryInfo.cs
@@ -7,7 +7,7 @@ namespace ApiAiSDK
 {
     internal static class LibraryInfo
     {
-        internal const string Version = "1.6.3";
+        internal const string Version = "1.6.4";
 
         internal const string Title = "ApiAiSDK";
         internal const string Description = "Library provides simple programming interface for making text and voice requests to the API.AI service.";
