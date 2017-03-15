109464c133422a71d7e1cf9418887a67b736b0b1
Danil Skachkov <danil.skachkov@speaktoit.com>
2017-03-15 17:51:51 +0700

    Remove unused import

--- ApiAiSDK/AIDataService.cs
+++ ApiAiSDK/AIDataService.cs
@@ -19,7 +19,6 @@
 // ***********************************************************************************************************************
 
 using System;
-using System.Configuration;
 using System.Net;
 using System.IO;
 using System.Linq;
